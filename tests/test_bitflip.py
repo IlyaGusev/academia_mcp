8645257efbb7dccf9109364ce60d0a35f9ce7e06
Ilya Gusev <phoenixilya@gmail.com>
2025-10-13 20:30:45 +0000

    fix

--- tests/test_bitflip.py
+++ tests/test_bitflip.py
@@ -35,18 +35,3 @@ async def test_bitflip_score_research_proposals_base() -> None:
     assert scores.proposals[1].strengths is not None
     assert scores.proposals[0].weaknesses is not None
     assert scores.proposals[1].weaknesses is not None
-
-
-async def test_bitflip_score_research_proposals_str() -> None:
-    arxiv_id = "2503.07826"
-    bit = (await extract_bitflip_info(arxiv_id)).bit
-    proposals = await generate_research_proposals(bit=bit, num_proposals=2)
-    scores = await score_research_proposals(proposals)
-    assert scores.proposals
-    assert len(scores.proposals) == 2
-    assert scores.proposals[0].spark is not None
-    assert scores.proposals[1].spark is not None
-    assert scores.proposals[0].strengths is not None
-    assert scores.proposals[1].strengths is not None
-    assert scores.proposals[0].weaknesses is not None
-    assert scores.proposals[1].weaknesses is not None
